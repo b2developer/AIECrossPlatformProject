1a785369fa6cc6acffbf054cc7aefdfece305a02
ASharpy <a.sharp3197@gmail.com>
2017-09-13 15:27:40 +1000

    Finished main Menu With Start and Quit Button

---
+++ AngryAliens/Main Menu Scene/StartButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    //Uses The index of the build order to load a scene when the start button is clicked
+  public void LoadSceneIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+	
+
+}
